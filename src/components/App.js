becff29d440b62c80c9925b63174041384904221
Cristina-F-R <cristina.faixedas@gmail.com>
2023-03-13 09:05:55 +0100

    added filter Quote

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 /* SECCIÓN DE IMPORT */
 
 // - De React
+import { useState } from 'react';
 // - Nuestros
 import sentences from '../data/sentences.json'
 // - Sass
@@ -10,13 +11,20 @@ import '../styles/App.scss';
 /* SECCIÓN DEL COMPONENTE */
 function App() {
   /* VARIABLES ESTADO (DATOS) */
-
+const [filterQuote, setFilterQuote] = useState(' ');
 
   /* FUNCIONES HANDLER */
+const handleQuoteFilter = (ev) =>{
+  setFilterQuote(ev.target.value);
 
+}
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
   const renderSentences = () =>{
-    return sentences 
+    return sentences
+    .filter((eachSentence) => {
+      return (eachSentence.quote.toLocaleLowerCase().includes(filterQuote.toLocaleLowerCase() ||
+      eachSentence.quote.toLocaleLowerCase().includes(filterQuote.toLocaleLowerCase())))
+    })
     .map((eachSentence, index) => (
       <li className='listLi' key={index}>
         <p className='ListLi__quote'>{eachSentence.quote} - </p> 
@@ -29,12 +37,25 @@ function App() {
   return <div className="App">
     <header>
       <h1>Frases de Friends</h1>
+      <form>
+        <label htmlFor="porFrase">
+          Filtrar por frase:
+        <input 
+        type="text" 
+        id="porFrase" 
+        placeholder='Escribe una palabra'
+        onChange={handleQuoteFilter} 
+        // value={}
+        />
+        </label>
+      </form>
+      </header>
       <main>
         <ul>
           {renderSentences()}
         </ul>
       </main>
-    </header>
+    
   </div>;
 }
 
